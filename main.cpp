36147b38590f5154da418adb91bfe86044724e16
Ariel Monterrosas <monterrosas78@csu.fullerton.edu>
2023-10-01 11:56:41 -0700

    KEYWORD, SEPARATORS work; INTGERS does not

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@ using namespace std;
 
 
 //Using regex for Regular Expressions
-const regex LITERAL("\"(?:[^\"\\\\]|\\\\.)*\"");
-const regex INTEGERS("\\d+");
-const regex IDENTIFIER("[a-zA-Z_][a-zA-Z0-9_]*");
-const regex KEYWORD ("(int|float|bool|true|false|if|else|then|endif|while|whileend|do|doend|for|forend|input|output|and|or|not)");
-const regex OPERATORS ("(\\+|\\-|\\*|\\/|\\%|\\=|\\<|\\>|\\<=|\\>=|\\==|\\!=)");
+const regex LITERAL("\\s*\"(?:[^\"\\\\]|\\\\.)*\"\\s*");
+const regex INTEGERS("\\s*\\d+\\s*");
+const regex IDENTIFIER("\\s*[a-zA-Z_][a-zA-Z0-9_]*\\s*");
+const regex KEYWORD("\\s*(int|float|bool|true|false|if|else|then|endif|while|whileend|do|doend|for|forend|input|output|and|or|not)\\s*");
+const regex OPERATORS("\\s*(\\+|\\-|\\*|\\/|\\%|\\=|\\<|\\>|\\<=|\\>=|\\==|\\!=)\\s*");
+const regex SEPARATORS("\\s*[\\(\\)\\{\\}\\[\\];,]\\s*");
 
-const regex SEPARATORS("[(){}\\[\\];,]");
 
 void lexer(istream& inputFile, ofstream& outPutFile) {
     smatch match;
@@ -29,30 +29,46 @@ void lexer(istream& inputFile, ofstream& outPutFile) {
             //Works
             if (regex_search(current, match, KEYWORD)){
                 outPutFile << "KEYWORD\t" << match[0] << endl;
+                cout << "Keyword\t" << match[0] << endl;
                 //cout << match.str(0) <<endl;
                 current = match.suffix().str();
+                continue;
 
             }
+            else if (regex_search(current, match, SEPARATORS)){
+                outPutFile << "SEPARATOR\t" << match[0] << endl;
+                cout << "Separator\t" << match[0] << endl;
+                current = match.suffix().str();
+                continue;
+            }
+
 
             //Works
             else if (regex_search(current, match, INTEGERS)){
                 outPutFile << "INTEGERS\t" << match[0] << endl;
                 //cout << match.str(0);
                 current = match.suffix().str();
+                continue;
             }
+
 
             //There is a bug here
-            else if (regex_search(current, match, OPERATORS)){
-                outPutFile << "OPERATORS\t" << match[0] << endl;
-                current= match.suffix().str();
+            // else if (regex_search(current, match, OPERATORS)){
+            //     outPutFile << "OPERATORS\t" << match[0] << endl;
+            //     current= match.suffix().str();
                 
-            }
+            // }
 
             //Bug here as well
-            else if (regex_search(current, match, SEPARATORS)){
-                outPutFile << "SEPARATORS\t" << match[0] << endl;
-                current = match.suffix().str();
-            }
+            // else if (regex_search(current, match, SEPARATORS)){
+            //     outPutFile << "SEPARATORS\t" << match[0] << endl;
+            //     current = match.suffix().str();
+            // }
+
+            // else if (regex_search(current, match, IDENTIFIER)){
+            //     outPutFile << "IDENTIFER\t" << match[0] << endl;
+            //     current = match.suffix().str();
+            // }
         }
      }
 
