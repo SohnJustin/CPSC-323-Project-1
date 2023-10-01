3cdf5858552c986f794301221fdbb3f21d64519a
Ariel Monterrosas <monterrosas78@csu.fullerton.edu>
2023-10-01 00:00:48 -0700

    Adding comments for issues to be solved later down the road

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ const regex INTEGERS("\\d+");
 const regex IDENTIFIER("[a-zA-Z_][a-zA-Z0-9_]*");
 const regex KEYWORD ("(int|float|bool|true|false|if|else|then|endif|while|whileend|do|doend|for|forend|input|output|and|or|not)");
 const regex OPERATORS ("(\\+|\\-|\\*|\\/|\\%|\\=|\\<|\\>|\\<=|\\>=|\\==|\\!=)");
+
 const regex SEPARATORS("[(){}\\[\\];,]");
 
 void lexer(istream& inputFile, ofstream& outPutFile) {
@@ -20,25 +21,34 @@ void lexer(istream& inputFile, ofstream& outPutFile) {
     string input;
 
 
+    //Test each case one by one to see if the Regular Expressions work
     while(getline(inputFile, input)){
         string current = input;
         while (!current.empty()){
+
+            //Works
             if (regex_search(current, match, KEYWORD)){
                 outPutFile << "KEYWORD\t" << match[0] << endl;
                 //cout << match.str(0) <<endl;
                 current = match.suffix().str();
 
             }
+
+            //Works
             else if (regex_search(current, match, INTEGERS)){
                 outPutFile << "INTEGERS\t" << match[0] << endl;
                 //cout << match.str(0);
                 current = match.suffix().str();
             }
+
+            //There is a bug here
             else if (regex_search(current, match, OPERATORS)){
                 outPutFile << "OPERATORS\t" << match[0] << endl;
                 current= match.suffix().str();
                 
             }
+
+            //Bug here as well
             else if (regex_search(current, match, SEPARATORS)){
                 outPutFile << "SEPARATORS\t" << match[0] << endl;
                 current = match.suffix().str();
@@ -52,12 +62,15 @@ void lexer(istream& inputFile, ofstream& outPutFile) {
 
 
 int main() {
+    //Reading and writing to files
     fstream input("input_scode.txt");
     ofstream output("output_scode.txt");
 
-    //will be using vectors
+    //Lexer function
     lexer(input, output);
 
+
+    //Closing files
     input.close();
     output.close();
 
