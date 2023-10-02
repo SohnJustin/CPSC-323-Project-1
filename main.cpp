6adc1dd64932487e12414b08f10341ec079f4e4a
Ariel Monterrosas <monterrosas78@csu.fullerton.edu>
2023-10-02 13:29:31 -0700

    Pushing P

--- main.cpp
+++ main.cpp
@@ -9,18 +9,22 @@ using namespace std;
 
 //Using regex for Regular Expressions
 //const regex LITERAL("\\s*\"(?:[^\"\\\\]|\\\\.)*\"\\s*");
+
+
 const regex DIGITS("[0-9]");
 const regex REAL ("[0-9]+(\\.[0-9]+)?");
 const regex IDENTIFIER("[a-zA-Z][a-zA-Z0-9]*");
-//const regex KEYWORD("\\s*(int|float|bool|true|false|if|else|then|endif|while|whileend|do|doend|for|forend|input|output|and|or|not)\\s*");
 const regex KEYWORD("(int|float|bool|true|false|if|else|then|endif|while|whileend|do|doend|for|forend|input|output|and|or|not)");
-const regex OPERATORS("\\s*(\\+|\\-|\\*|\\/|\\%|\\=|\\<|\\>|\\<=|\\>=|\\==|\\!=)\\s*");
+//const regex OPERATORS("\\s*(\\+|\\-|\\*|\\/|\\%|\\=|\\<|\\>|\\<=|\\>=|\\==|\\!=)\\s*");
+//const regex OPERATORS("(\\+|\\-|\\*|\\/|\\%|\\=|\\<|\\>|\\<=|\\>=|\\==|\\!=)");
+const regex OPERATORS("(\\+|\\-|\\*|\\/|\\%|\\=|\\<|\\>|\\<=|\\>=|\\==|\\!=)");
 const regex SEPARATORS("\\s*[\\(\\)\\{\\}\\[\\];,]\\s*");
 
 
 void lexer(istream& inputFile, ofstream& outPutFile) {
     smatch match;
     string input;
+    int i = 0;
 
     outPutFile << "TOKEN\t\tLEXEMES\n";
 
@@ -29,40 +33,48 @@ void lexer(istream& inputFile, ofstream& outPutFile) {
     while(getline(inputFile, input)){
         string current = input;
         while (!current.empty()){
+            cout << current << endl;
+            cout << i++ << endl;
 
-            //Trimming part
-            input = regex_replace(input, regex("^\\s+|\\s+$"), "");
-
-            //Real Numbers
-            if (regex_search(current, match, REAL)){
-                outPutFile << "REAL\t\t" << match[0] << endl;
-                current = match.suffix().str();
-                cout << "Real\t\t" << match[0] << endl;
-                continue;
-            }
             //Keywords
-            else if (regex_search(current, match, KEYWORD)){
+            if (regex_search(current, match, KEYWORD)){
                 outPutFile << "KEYWORD\t\t" << match[0] << endl;
-                cout << "Keyword\t\t" << match[0] << endl;
-                //cout << "Hello\n";
+                cout << "\nKeyword\t\t" << match[0] << endl;
                 current = match.suffix().str();
+                //cout << current << endl << endl;
                 continue;
 
             }
             //SEPARATORS
             else if (regex_search(current, match, SEPARATORS)){
                 outPutFile << "SEPARATOR\t" << match[0] << endl;
-                cout << "Separator\t" << match[0] << endl;
+                cout << "\nSeparator\t" << match[0] << endl;
                 current = match.suffix().str();
+                //cout << current << endl << endl;
+                continue;
+            }
+
+            //Letters
+            else if (regex_search(current, match, IDENTIFIER)){
+                cout << "Testing if in IDENTIFIER";
+                outPutFile << "IDENTIFIER\t" << match[0] << endl;
+                cout << "\nIdentifier\t" << match[0] << endl;
+                current = match.suffix().str();
+                //cout << current << endl << endl;
+                continue;
+            }
+
+            else if (regex_search(current, match, OPERATORS)){
+                outPutFile << "OPERATOR\t" << match[0] << endl;
+                cout << "\nOPERATOR\t" << match[0] << endl;
+                current = match.suffix().str();
+                //cout << current << endl << endl;    //Printing out current line to parse
                 continue;
             }
             
-
-            
-
             else
-                current = "";
-                break;
+                current = current.substr(1);
+                continue;
 
         }
     }
